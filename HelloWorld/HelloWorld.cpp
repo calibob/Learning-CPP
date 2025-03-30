6ead846107d705bdb1c2407856f8153daa7dd864
calibob <yassinezairi242@gmail.com>
2025-03-30 14:50:56 +0000

    Initial commit

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,53 @@
+using namespace std;
+#include <iostream> // input output stream
+
+int main() // entry point of the application
+{
+	/*int age = 30;
+	float money = 145.202; // 4 bytes
+	double scientific = 15162654681515975.646168; // 8 bytes
+
+	char name = 'Y';
+	char name1[8] = "yassine"; // Y A S S I N E `EOL` 'end of line'
+	string name2 = "ZAIRI";
+
+	bool isAngry = false;
+	cout << isAngry;*/
+
+	/*int x;
+	int y;
+	string name;
+
+	cout << "Enter the first number:";
+	cin >> x;
+	cout << "Enter the second number:";
+	cin >> y;
+	cout << "The sum of the numbers is:\n" << x + y;
+
+
+	cout << "Enter your name:\n";
+	cin >> name;
+	cout << "The name you entered is: " << name;*/
+
+
+	int x = 2;
+	int y = 3;
+
+	int sum = x + y;
+	cout << "Sum = " << x + y << endl;
+
+	int difference = x - y;
+	cout << "subtraction = " << difference << endl;
+
+	int product = x * y;												
+	cout << "Product = " << product << endl;
+
+	double division = x / y;
+	cout << "division = " << division << endl;
+
+	int remainder = x % y;																		
+	cout << "Remainder = " << remainder << endl;
+
+}
+
+
